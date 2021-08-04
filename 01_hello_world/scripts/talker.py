08c227d8172cbb33e0c2839cc6973d1b7eaa1ac9
li <li864061219@163.com>
2021-08-04 17:03:59 +0800

    first commit

---
+++ 01_hello_world/scripts/talker.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+class talker():
+    def __init__(self):
+        rospy.init_node("talker", anonymous=False)
+        rospy.on_shutdown(self.shutdown)
+        self.pub = rospy.Publisher("/hello_msg", String, queue_size=5)
+        self.r = rospy.Rate(1)
+
+        hello = String()
+        start = rospy.get_time()
+        while not rospy.is_shutdown():
+            now = rospy.get_time()
+            hello.data = 'hello world %.2f s' % (now - start)
+            self.pub.publish(hello)
+            self.r.sleep()
+
+    def shutdown(self):
+        
+        pass
+
+if __name__ == "__main__":
+    obj = talker()
+    rospy.spin()
